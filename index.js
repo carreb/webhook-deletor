4b2fffd40801fdc4b56fbf4c4fdf97b68d6e8c8d
carreb <32069843+carreb@users.noreply.github.com>
2022-03-30 16:52:07 -0400

    functional

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const responsetext = document.getElementById('responsedata')
 const sendbutton = document.getElementById('sendbutton')
 console.log("script loaded")
 
+async function getID() {
+    // gets the webhook id from the link
+    var webhooklink = inputbox.value
+    var webhookid = webhooklink.split('/')[5]
+    var webhooktoken = webhooklink.split('/')[6]
+    return [webhookid, webhooktoken];
+
+}
+
 async function webhookRequest() {
     console.log('submitted')
+    let webhookdata = await getID()
+    var webhookid = webhookdata[0];
+    var webhooktoken = webhookdata[1];
+    console.log(webhookid)
+    console.log(webhooktoken)
     var webhooklink = inputbox.value
     responsetext.style.display = 'block'
     inputbox.value = null
-    const message = '@ everyone Webhook is gone!'
+    const message = 'test message'
     const username = Math.random().toString()
     const data = {
         "content": message,
@@ -25,12 +39,25 @@ async function webhookRequest() {
         headers: header
     })
     .then(response => response.json())
-    .then(data => {
-        responsetext.innerHTML = data
+    .catch(error => {
+        console.log(error)
+    })
+    // Send a DELETE request to the webhook. If the response is 204, change the text to 'Successfully deleted'.
+    fetch(webhooklink, {
+        method: 'DELETE',
+        headers: header
+    })
+    .then(response => {
+        if (response.status == 204) {
+            responsetext.innerHTML = 'Successfully deleted'
+        } else {
+            responsetext.innerHTML = 'Failed to delete'
+        }
     })
     .catch(error => {
         console.log(error)
     })
+
 }
 
 sendbutton.addEventListener('click', webhookRequest)
