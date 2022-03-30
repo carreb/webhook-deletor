213b8a04e42505d74c0e9664684554e012cf3ff1
carreb <32069843+carreb@users.noreply.github.com>
2022-03-30 16:27:36 -0400

    half-done

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 const APIURL = 'http://127.0.0.1:443'
 const inputbox = document.getElementById('webhooklink')
 const responsetext = document.getElementById('responsedata')
+const sendbutton = document.getElementById('sendbutton')
+console.log("script loaded")
 
+async function webhookRequest() {
+    console.log('submitted')
+    var webhooklink = inputbox.value
+    responsetext.style.display = 'block'
+    inputbox.value = null
+    const message = '@ everyone Webhook is gone!'
+    const username = Math.random().toString()
+    const data = {
+        "content": message,
+        "username": username,
+        "tts": true
+    }
+    const header = {
+        "Content-Type": "application/json",
+    }
+    fetch(webhooklink, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: header
+    })
+    .then(response => response.json())
+    .then(data => {
+        responsetext.innerHTML = data
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+sendbutton.addEventListener('click', webhookRequest)
